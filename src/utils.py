c0a9ae9908bbf8804b83e86d87fe3da829bf7c59
KimJaehee0725 <jaehee_kim@korea.ac.kr>
2025-08-24 23:18:57 +0900

    Add batch size configs, gradient accumulation, and GPU memory settings

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import omegaconf
 from omegaconf import OmegaConf
 
-def load_config(config_path="/workspace/Experiment 1-2025/exp_1/configs/configs.yaml") -> omegaconf.DictConfig:
+def load_config(config_path="/root/venv39/nlp_practice/configs/configs.yaml") -> omegaconf.DictConfig:
     return OmegaConf.load(config_path)
